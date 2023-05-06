269088c322c99b796b19ef734cc50d0e7c260d92
Stanislav Kintoriak <kintoriak@gmail.com>
2023-05-06 22:47:02 +0300

    commit working
    1) finished  components:
    Statistics, Notification, FeedbackOptions, Section;
    2) added simple styles;

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,58 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  feedbackBtnClick = e => {
+    const eValue = e;
+
+    this.setState((prevState, { step }) => {
+      return { ...prevState, [eValue]: prevState[eValue] + step };
+    });
+  };
+
+  countTotalFeedback() {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    return (this.state.good / this.countTotalFeedback()) * 100;
+  }
+
+  render() {
+    return (
+      <>
+        <Section
+          title="Please leave your feedback"
+          FeedbackOptions={
+            <FeedbackOptions
+              options={['good', 'neutral', 'bad']}
+              clickFeedback={this.feedbackBtnClick}
+            />
+          }
+          Statistics={
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          }
+        />
+        </>
+    );
+  }
+}
+
+App.propTypes = {
+  step: PropTypes.number.isRequired,
 };
