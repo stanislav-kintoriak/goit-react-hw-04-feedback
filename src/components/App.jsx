b618dffc42c89cebb6af1061e46b567c50565ff7
Stanislav Kintoriak <kintoriak@gmail.com>
2023-05-11 21:11:42 +0300

    Commit - fix of the props (keys) in the App.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -34,7 +34,7 @@ export class App extends Component {
           title="Please leave your feedback"
           FeedbackOptions={
             <FeedbackOptions
-              options={['good', 'neutral', 'bad']}
+              options={Object.keys(this.state)}
               onLeaveFeedback={this.feedbackBtnClick}
             />
           }
